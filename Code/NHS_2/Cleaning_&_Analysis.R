88e5e3e52077c4b4e3025a0290b60dba1be6d70e
Soodi <vesta.2300@gmail.com>
2019-07-02 14:31:10 -0400

    Update Cleaning_&_Analysis.R

--- Code/NHS_2/Cleaning_&_Analysis.R
+++ Code/NHS_2/Cleaning_&_Analysis.R
@@ -14,6 +14,7 @@ library(adegenet)
 library(glmnet)
 library(fmsb)
 
+# Cleaning
 addToBase <- function(base_formula, adjustingVariables) {
   for (var in adjustingVariables) {
     base_formula <- update.formula( base_formula, as.formula(sprintf('~ . + %s', var)) )
@@ -33,123 +34,24 @@ boxcox_trans <- function(df, temp_var){
 }
 
 
-
-cont_ewas <- function(data, depvar, time1, time2,  covars , adjvars){
-  c <- 0
-
-
-  out_df <- data.frame(matrix(ncol = 10, nrow = length(covars)))#, row.names = b)
-
-
-  row.names(out_df) <- covars
-  colnames(out_df) <- c('coef','exp_coef', 'se_coef', 'z', 'p_val','LB','UB','stat_power','PH_pval', 'VIF')
-  #beta_time <- list()
-  # for power calculation
-  alpha <- 0.05
-  n <- length(unique(data$id))
-  psi<- 0.001667384 #sum(data$chdcase==1)/(2*n-sum(data$chdcase==1))/2
-  ua2<-qnorm(1-alpha/2)
-
-  for (i in covars){
-
-
-    c <- c+1
-
-    baseform <- NULL
-    doForm <- NULL
-    baseform <- as.formula(sprintf('Surv(%s, %s, %s) ~ %s', time1, time2, depvar, i))
-    doForm <- addToBase(baseform, adjvars)
-
-    mod <- coxph(formula = doForm, data = data)
-    # print(mod)
-
-
-    out_df[i,]['coef'] <- summary(mod)$coefficients[1,][1]
-    out_df[i,]['exp_coef'] <- summary(mod)$coefficients[1,][2]
-    out_df[i,]['se_coef'] <- summary(mod)$coefficients[1,][3]
-    out_df[i,]['z'] <- summary(mod)$coefficients[1,][4]
-    out_df[i,]['p_val'] <- summary(mod)$coefficients[1,][5]
-    out_df[i,]['LB'] <- summary(mod)$conf.int[1,][3]
-    out_df[i,]['UB'] <- summary(mod)$conf.int[1,][4]
-
-
-    zp <- cox.zph(mod)
-    #beta_time[[i]] <- zp[1]
-    out_df[i,]['PH_pval'] <- zp$table[1,3]
-
-    #power
-    ## overal incidence prop
-    theta <- summary(mod)$coefficients[1,][2]
-    formula_base <- as.formula(sprintf('%s ~ %s', i,paste(adjvars, collapse= "+") ))
-    #formula_base <- as.formula(sprintf('%s ~ %s', i,paste(covars[! covars %in% c(i)], collapse= "+") ))
-    sigma2<-var(data[i])
-    res.lm<-lm(formula = formula_base, data = data)
-    tt<-summary(res.lm)
-    rho2<-tt$r.squared
-    part2<-n*(log(theta))^2*sigma2*psi*(1-rho2)
-    power<-pnorm(sqrt(part2)-ua2)
-    res<-list(power=power, rho2=rho2, sigma2=sigma2, psi=psi)
-    out_df[i,]['stat_power'] <- res$power
-    #print(c)
-
-    formula_base <- NULL
-    formula_base <- as.formula(sprintf('%s ~ %s', i,paste(adjvars, collapse= "+") ))
-    res.lm<-lm(formula = formula_base, data = data)
-    out_df[i,]['VIF'] <- VIF(res.lm)
-  }
-  #output <- list("out_df" = out_df, "beta_time" = beta_time)
-  return(out_df)
-}
-
-
-
-df <- read_sas('./DATA/nhs2_data.sas7bdat')    # 990363
-df <- subset(df, start_time < stop_time)    # no change
-
-
-
-
-df <- categorize_smk(df, df, 'harvard')
-df$hbpcon <- ifelse(is.na(df$hbpcon), 0, df$hbpcon)
-df$cholcon <- ifelse(is.na(df$cholcon), 0, df$cholcon)
-df$aspicon <- ifelse(df$aspicon == 9, 0, df$aspicon)
-df$veyncon <- ifelse(is.na(df$veyncon), 0, df$veyncon)
-df$mvitcon <- ifelse(is.na(df$mvitcon) , 0, df$mvitcon)
-df$mvitcon <- ifelse(df$mvitcon==2 , 1, 0)
-
+df <- read_sas('data.sas7bdat')    
 
 indx <- apply(df, 2, function(x) any(is.nan(x)| is.na(x) | is.infinite(x)))
 names(df)[indx]
 
 df <- na.locf(df)
 
-indx <- apply(df, 2, function(x) any(is.nan(x)| is.na(x) | is.infinite(x)))
-names(df)[indx]
 
 
-
-raw_adjustfor <- c('white', 'mifh', 'hbpfh', 'agecon', 'bmicon', 'smkdrcon', 'hbpcon',
-                   'cholcon', 'aspicon', 'mvitcon', 'nhorcon', 'actcon', 'veyncon', 'calorconvconv')
-
-covar <- c("wwinevconv",	"dressvconv",	"yogvconv",	"rcarvconv",	"liqvconv",	"rwinevconv",
-           "donutvconv",	"hotdogvconv",	"whbrvconv",	"ajvconv",	"pnutvconv",
-           "allprocvconv",	"hambuvconv",	"cervconv",	"sugbevvconv",	"cookvconv",
-           "raisvconv",	"bmanvconv",	"branaconvconv",	"t161vconv",
-           "addfat_liqvconv",	"addfat_solvconv",	"f161vconv",	"afatvconv",	"f180vconv",
-           "mill_grnvconv",	"phytvconv",	"navconv",	"hydprovconv",	"satvconv",
-           "uisorvconv",	"sum_whgcarbvconv",	"cerafvconv",	"t182vconv",	"uttocovconv",
-           "f160vconv",	"fol_nvconv",	"ubT3vconv",	"mufa_plvconv",	"germnvconv",
-           "uapigvconv",	"ubtocovconv",	"brannvconv",	"sevconv",	"f140vconv",
-           "mn_nvconv",	"uaT3vconv",	"cholvconv",	"vcfolicvconv",	"t181vconv",
-           "mnvconv",	"b6svconv",	"hemevconv",	"es2vconv",	"trnssvconv", "mufa_avconv", "alcovconv")
+raw_adjustfor <- c(...)   # this vector should be filled with adjusting variables, such as age, gender, race, etc..
+covar <- c(...)   # this vector should be filled with exposure names.
 
 all_var <- c(raw_adjustfor, covar, 'id', 'chdcase', 'start_time', 'stop_time')
-
 df <- df[, (names(df) %in% all_var)]
 
 temp_var <- c(covar, 'calorconvconv')
 
-
+# parallel computing
 no_cores <- detectCores() - 2
 cl<-makeCluster(no_cores)
 registerDoParallel(cl)
@@ -167,10 +69,8 @@ trans_df <- foreach(temp_var = temp_var,
 stopCluster(cl)
 stopImplicitCluster()
 
-
-
-add_var <- c('id', 'chdcase', 'start_time', 'stop_time', 'white', 'mifh', 'hbpfh', 'agecon', 'bmicon', 'smkdrcon', 'hbpcon',
-             'cholcon', 'aspicon', 'mvitcon', 'nhorcon', 'actcon', 'veyncon')
+add_var <- c('id', ...)
+              
 for (i in add_var){
   trans_df[[i]] <- df[[i]]
 }
@@ -184,17 +84,67 @@ for (i in temp_var){
   c <- c+1
 }
 
+write_sas(z_trans_df,'cleaned_data.sas7bdat')
+              
+# Analysis
 
-write_sas(z_trans_df,'./DATA/cleaned_nhs2_data.sas7bdat')
+# this function associates an exposure with disease status, while controlling for adjusting variables, using Cox model.
+cont_ewas <- function(data, depvar, time1, time2,  covars , adjvars, alpha, psi){
+  c <- 0
+  out_df <- data.frame(matrix(ncol = 10, nrow = length(covars)))#, row.names = b)
+  row.names(out_df) <- covars
+  colnames(out_df) <- c('coef','exp_coef', 'se_coef', 'z', 'p_val','LB','UB','stat_power','PH_pval', 'VIF')
+
+  for (i in covars){
+
+    c <- c+1
+
+    baseform <- NULL
+    doForm <- NULL
+    baseform <- as.formula(sprintf('Surv(%s, %s, %s) ~ %s', time1, time2, depvar, i))
+    doForm <- addToBase(baseform, adjvars)
+    mod <- coxph(formula = doForm, data = data)
 
 
+    out_df[i,]['coef'] <- summary(mod)$coefficients[1,][1]
+    out_df[i,]['exp_coef'] <- summary(mod)$coefficients[1,][2]
+    out_df[i,]['se_coef'] <- summary(mod)$coefficients[1,][3]
+    out_df[i,]['z'] <- summary(mod)$coefficients[1,][4]
+    out_df[i,]['p_val'] <- summary(mod)$coefficients[1,][5]
+    out_df[i,]['LB'] <- summary(mod)$conf.int[1,][3]
+    out_df[i,]['UB'] <- summary(mod)$conf.int[1,][4]
 
-adjustfor <- c('factor(white)', 'factor(mifh)', 'factor(hbpfh)', 'agecon', 'bmicon',
-                   'factor(smkdrcon)', 'hbpcon',
-                   'factor(cholcon)', 'factor(aspicon)', 'factor(mvitcon)',
-                   'factor(nhorcon)', 'actcon', 'factor(veyncon)', 'calorconvconv')
+
+    zp <- cox.zph(mod)
+    out_df[i,]['PH_pval'] <- zp$table[1,3]
+
+    # power calculation
+    n <- length(unique(data$id))
+    ua2<-qnorm(1-alpha/2)
+    theta <- summary(mod)$coefficients[1,][2]
+    formula_base <- as.formula(sprintf('%s ~ %s', i,paste(adjvars, collapse= "+") ))
+    sigma2<-var(data[i], na.rm = TRUE)
+    res.lm<-lm(formula = formula_base, data = data)
+    tt<-summary(res.lm)
+    rho2<-tt$r.squared
+    part2<-n*(log(theta))^2*sigma2*psi*(1-rho2)
+    power<-pnorm(sqrt(part2)-ua2)
+    res<-list(power=power, rho2=rho2, sigma2=sigma2, psi=psi)
+    out_df[i,]['stat_power'] <- res$power
+
+    # VIF calculation
+    formula_base <- NULL
+    formula_base <- as.formula(sprintf('%s ~ %s', i,paste(adjvars, collapse= "+") ))
+    res.lm<-lm(formula = formula_base, data = data)
+    out_df[i,]['VIF'] <- VIF(res.lm)
+  }
+  return(out_df)
+}
 
 
+adjustfor <- c(...)
+
+# parallel computing
 no_cores <- detectCores() - 2
 cl<-makeCluster(no_cores)
 registerDoParallel(cl)
