7f0b18c3d7d3628492ee4ef4b7dabc89c07c43d9
Soodi <vesta.2300@gmail.com>
2019-09-13 13:35:48 -0400

    Update EWAS.R

--- Code/NHS_1/EWAS.R
+++ Code/NHS_1/EWAS.R
@@ -1,3 +1,5 @@
+# load required libraries
+
 library(car)
 library(bestNormalize)
 library(data.table)
@@ -14,37 +16,47 @@ library(adegenet)
 library(glmnet)
 library(fmsb)
 
-#' Extend the base formula with variables we are adjusting for.
-#'
-#' @param base_formula a formula object with the dependent variable as a function of the independent variable
-#' @param adjustingVariables a character vector of column names from the data.frame to adjust for
-#'
-#' @return An updated formula object representing a base formula with adjustment variables.
 
-addToBase <- function(base_formula, adjustingVariables) {
+# functoin  >> to extend the base formula with variables we are adjusting for
+# @arguments >> base_formula: a formula object with the dependent variable as a function of the independent variable
+# @arguments >> adjustingVariables: a character vector of column names from the data.frame to adjust for
+# @return    >> an updated formula object representing a base formula with adjustment variables
+ 
+addToBase <- function(base_formula, adjustingVariables) 
+{
   for (var in adjustingVariables) {
     base_formula <- update.formula( base_formula, as.formula(sprintf('~ . + %s', var)) )
   }
-  return(base_formula) }
+  return(base_formula) 
+}
 
-# this function associates an exposure with disease status, while controlling for adjusting variables, using Cox model.
-cont_ewas <- function(data, depvar, time1, time2,  covars , adjvars, alpha, psi){
-  c <- 0
-  out_df <- data.frame(matrix(ncol = 10, nrow = length(covars)))
+
+# function  >> to associate an exposure with disease status, while controlling for adjusting variables, using Cox model
+# @arguments >> data: a dataframe containing the cohort data
+# @arguments >> depvar: dependent variable in the survival analysis
+# @arguments >> time1: the variable showing the start time of an observation
+# @arguments >> time2: the variable showing the end time of an observation
+# @arguments >> covars: exposure of interest that we want to find its effect on a disease risk
+# @arguments >> adjvars: a vector containing the variables that we want to adjust our analysis for
+# @return    >> a dataframe containing the result of Cox model, proportional hazard assumption, and VIF for an exposure of interest
+
+cont_ewas <- function(data, depvar, time1, time2,  covars , adjvars)
+{
+  # initiate a dataframe to store the results in
+  out_df <- data.frame(matrix(ncol = 9, nrow = length(covars)))
   row.names(out_df) <- covars
-  colnames(out_df) <- c('coef','exp_coef', 'se_coef', 'z', 'p_val','LB','UB','stat_power','PH_pval', 'VIF')
+  colnames(out_df) <- c('coef','exp_coef', 'se_coef', 'z', 'p_val','LB','UB','PH_pval', 'VIF')
 
-  for (i in covars){
-
-    c <- c+1
-
+  for (i in covars)
+  {
     baseform <- NULL
     doForm <- NULL
     baseform <- as.formula(sprintf('Surv(%s, %s, %s) ~ %s', time1, time2, depvar, i))
     doForm <- addToBase(baseform, adjvars)
     mod <- coxph(formula = doForm, data = data)
 
-
+    # collect the estimated effect size by Cox model, hazard ratio, standard deviation of the estimated effect,
+    # z-statistic, p-value, lower and upper bound estimated for an effect.
     out_df[i,]['coef'] <- summary(mod)$coefficients[1,][1]
     out_df[i,]['exp_coef'] <- summary(mod)$coefficients[1,][2]
     out_df[i,]['se_coef'] <- summary(mod)$coefficients[1,][3]
@@ -53,24 +65,12 @@ cont_ewas <- function(data, depvar, time1, time2,  covars , adjvars, alpha, psi)
     out_df[i,]['LB'] <- summary(mod)$conf.int[1,][3]
     out_df[i,]['UB'] <- summary(mod)$conf.int[1,][4]
 
-    # checking for the proportional hazard assumption
+    # check for the proportional hazard assumption by assessing the correlation between 
+    # the Schoenfeld residuals and survival time
     zp <- cox.zph(mod)
     out_df[i,]['PH_pval'] <- zp$table[1,3]
 
-    # power calculation
-    n <- length(unique(data$id))
-    ua2<-qnorm(1-alpha/2)
-    theta <- summary(mod)$coefficients[1,][2]
-    formula_base <- as.formula(sprintf('%s ~ %s', i,paste(adjvars, collapse= "+") ))
-    sigma2<-var(data[i], na.rm = TRUE)
-    res.lm<-lm(formula = formula_base, data = data)
-    tt<-summary(res.lm)
-    rho2<-tt$r.squared
-    part2<-n*(log(theta))^2*sigma2*psi*(1-rho2)
-    power<-pnorm(sqrt(part2)-ua2)
-    res<-list(power=power, rho2=rho2, sigma2=sigma2, psi=psi)
-    out_df[i,]['stat_power'] <- res$power
-
+    # check for multi-collinearity
     # VIF calculation
     formula_base <- NULL
     formula_base <- as.formula(sprintf('%s ~ %s', i,paste(adjvars, collapse= "+") ))
@@ -82,11 +82,13 @@ cont_ewas <- function(data, depvar, time1, time2,  covars , adjvars, alpha, psi)
 
 
 # read data
-data_path <- './data.sas7bdat'
-data <- read_sas(data_path)
+data <- read_sas('cleaned_data_sample.sas7bdat')
 
-covar <- c(...)     # this vector should be filled with exposure names.
-adjustfor <- c(...)  # this vector should be filled with adjusting variables, such as age, gender, race, etc..
+# this vector should be filled with exposure names.
+covar <- c('fat_intake', 	'protein_intake'	'vitamin_intake'	'fruit_intake'	'meat_intake')  
+
+# this vector should be filled with adjusting variables.
+adjustfor <- c('physical_activity',	'calorie_intake')  
 
 # parallel computing
 no_cores <- detectCores() - 2
