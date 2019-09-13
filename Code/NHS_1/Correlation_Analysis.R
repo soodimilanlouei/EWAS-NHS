1b6260595308dbfc13a33a4bf58740ed18195c2d
Soodi <vesta.2300@gmail.com>
2019-09-13 14:14:18 -0400

    Update Correlation_Analysis.R

--- Code/NHS_1/Correlation_Analysis.R
+++ Code/NHS_1/Correlation_Analysis.R
@@ -1,3 +1,5 @@
+# load required libraries
+
 library(car)
 library(bestNormalize)
 library(data.table)
@@ -18,13 +20,24 @@ library(corrplot)
 library(Hmisc)
 library(corrplot)
 
-data <- read_sas('./data.sas7bdat')
-sig_covar <- c(...)     # this vector should be filled with statisitcally significant variables found by EWAS.
 
+# read data
+data <- read_sas('data_sample.sas7bdat')
 
+# this vector should be filled with statisitcally significant variables found by EWAS.
+sig_covar <- c('protein_intake', 'fruit_intake')     
+
+# calculate the correlation among significant exposures
 cor_res <- rcorr(as.matrix(data), type = 'spearman')
 
-flattenCorrMatrix <- function(cormat, pmat) {
+
+# functoin  >> to flatten the correlation matrix along with the associated p-values
+# @arguments >> cormat: a correlation matrix
+# @arguments >> pmat: p-value matrix associated with the correlation matrix
+# @return    >> a flattened dataframe with the names of the paired correlation and the associated p-value in each row
+
+flattenCorrMatrix <- function(cormat, pmat) 
+{
   ut <- upper.tri(cormat)
   data.frame(
     row = rownames(cormat)[row(cormat)[ut]],
@@ -34,27 +47,24 @@ flattenCorrMatrix <- function(cormat, pmat) {
   )
 }
 
+
 cor_res_flatten <- flattenCorrMatrix(cor_res$r, cor_res$P)
 write.csv(cor_res_flatten, './cor_res_flatten.csv')
 
-
-out_df <- data.frame(matrix(ncol = 3, nrow = 1378))
+# permutation to find null p-value of each correlation pair
+out_df <- data.frame(matrix(ncol = 3, nrow = nrow(cor_res_flatten))
 colnames(out_df) <- c('row', 'column', 'perm_cor')
 
-# permutation
 c <- 1
-cc <- 1
 for (i in sig_covar[1:52]){
-  cc <- cc+1
-  for (j in sig_covar[cc:53]){
+  c <- c+1
+  for (j in sig_covar[c:53]){
     if (i != j){
       temp <- as.matrix(sub_merge[c(i,j)])
       temp[,2] <- sample(temp[,2])
       out_df[c,]['row'] <- i
       out_df[c,]['column'] <- j
       out_df[c,]['perm_cor'] <- rcorr(temp, type = 'spearman')$r[2]
-      c <- c+1
-      print(c)
     }
   }
 }
@@ -62,7 +72,6 @@ for (i in sig_covar[1:52]){
 
 out_df <- out_df[
   with(out_df, order(row, column)),
-  ]
 
 
 cor_res_flatten <- cor_res_flatten[
@@ -82,6 +91,7 @@ write.csv(cor_merge_2, './permuted_correlation.csv')
 
 
 # visualization
-thre <- 5.47E-05      # threshold is found using the permutation procedure and manually inserted here for the visualization
+# threshold is found using the permutation procedure and manually inserted here for the visualization
+thre <- 5.47E-05      
 corrplot(cor_res$r,  order="hclust", tl.cex = 0.5, tl.col = 'black', type="upper",
          p.mat = cor_res$P, sig.level = thre, insig = "pch", pch.cex = 0.5)
